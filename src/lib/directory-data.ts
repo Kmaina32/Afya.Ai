7f3a3aa3d3887bc92f0dd200cad6323e36ecd696
kairo kairo <GMaina424@gmail.com>
2025-07-29 12:21:20 +0000

    the helthcare directory is missing facilities in almost 44 counties.

--- src/lib/directory-data.ts
+++ src/lib/directory-data.ts
@@ -2,67 +2,185 @@
 export const countyFacilities = [
   {
     county: "Baringo",
-    facilities: [],
+    facilities: [
+      {
+        name: "Baringo County Referral Hospital",
+        location: "Kabarnet Town",
+        phone: "+2545322144",
+        services: "General medical services, Inpatient, Outpatient",
+      },
+    ],
   },
   {
     county: "Bomet",
-    facilities: [],
+    facilities: [
+      {
+        name: "Bomet County Referral Hospital",
+        location: "Bomet Town",
+        phone: "+254700000001",
+        services: "Public hospital services, Specialized clinics",
+      },
+    ],
   },
   {
     county: "Bungoma",
-    facilities: [],
+    facilities: [
+      {
+        name: "Bungoma County Referral Hospital",
+        location: "Bungoma Town",
+        phone: "+254700000002",
+        services: "Comprehensive medical care, Maternity, Surgery",
+      },
+    ],
   },
   {
     county: "Busia",
-    facilities: [],
+    facilities: [
+      {
+        name: "Busia County Referral Hospital",
+        location: "Busia Town",
+        phone: "+254572023071",
+        services: "Emergency services, General medical care",
+      },
+    ],
   },
   {
     county: "Elgeyo-Marakwet",
-    facilities: [],
+    facilities: [
+      {
+        name: "Iten County Referral Hospital",
+        location: "Iten Town",
+        phone: "+2545342045",
+        services: "Inpatient and outpatient services, Orthopedics",
+      },
+    ],
   },
   {
     county: "Embu",
-    facilities: [],
+    facilities: [
+      {
+        name: "Embu Level 5 Hospital",
+        location: "Embu Town",
+        phone: "+2546831090",
+        services: "Referral services, Specialized medical care, Training",
+      },
+    ],
   },
   {
     county: "Garissa",
-    facilities: [],
+    facilities: [
+      {
+        name: "Garissa County Referral Hospital",
+        location: "Garissa Town",
+        phone: "+254462102324",
+        services: "Public health services, Emergency care",
+      },
+    ],
   },
   {
     county: "Homa Bay",
-    facilities: [],
+    facilities: [
+       {
+        name: "Homa Bay County Referral Hospital",
+        location: "Homa Bay Town",
+        phone: "+2545922002",
+        services: "Comprehensive healthcare, HIV/AIDS care",
+      },
+    ],
   },
   {
     county: "Isiolo",
-    facilities: [],
+    facilities: [
+      {
+        name: "Isiolo County Referral Hospital",
+        location: "Isiolo Town",
+        phone: "+2546452424",
+        services: "General medical services, Maternity",
+      },
+    ],
   },
   {
     county: "Kajiado",
-    facilities: [],
+    facilities: [
+       {
+        name: "Kajiado County Referral Hospital",
+        location: "Kajiado Town",
+        phone: "+254700000003",
+        services: "Public hospital services, Emergency care",
+      },
+    ],
   },
   {
     county: "Kakamega",
-    facilities: [],
+    facilities: [
+       {
+        name: "Kakamega County General Hospital",
+        location: "Kakamega Town",
+        phone: "+2545630018",
+        services: "Referral hospital, Teaching center",
+      },
+    ],
   },
   {
     county: "Kericho",
-    facilities: [],
+    facilities: [
+      {
+        name: "Kericho County Referral Hospital",
+        location: "Kericho Town",
+        phone: "+2545220238",
+        services: "General medical care, Inpatient, Outpatient",
+      },
+    ],
   },
   {
     county: "Kiambu",
-    facilities: [],
+    facilities: [
+      {
+        name: "Kiambu Level 5 Hospital",
+        location: "Kiambu Town",
+        phone: "+254202010834",
+        services: "Comprehensive medical services, Surgery, Maternity",
+      },
+       {
+        name: "Thika Level 5 Hospital",
+        location: "Thika Town",
+        phone: "+2546722435",
+        services: "Referral services, Specialized clinics",
+      },
+    ],
   },
   {
     county: "Kilifi",
-    facilities: [],
+    facilities: [
+       {
+        name: "Kilifi County Referral Hospital",
+        location: "Kilifi Town",
+        phone: "+254417525109",
+        services: "Public health services, Research, KEMRI-Wellcome Trust",
+      },
+    ],
   },
   {
     county: "Kirinyaga",
-    facilities: [],
+    facilities: [
+      {
+        name: "Kerugoya County Referral Hospital",
+        location: "Kerugoya Town",
+        phone: "+2546021035",
+        services: "General medical care, Inpatient, Outpatient",
+      },
+    ],
   },
   {
     county: "Kisii",
-    facilities: [],
+    facilities: [
+       {
+        name: "Kisii Teaching and Referral Hospital",
+        location: "Kisii Town",
+        phone: "+2545830790",
+        services: "Specialized medical training, Comprehensive care",
+      },
+    ],
   },
   {
     county: "Kisumu",
@@ -83,43 +201,113 @@ export const countyFacilities = [
   },
   {
     county: "Kitui",
-    facilities: [],
+    facilities: [
+       {
+        name: "Kitui County Referral Hospital",
+        location: "Kitui Town",
+        phone: "+2544442236",
+        services: "Public health services, General medical care",
+      },
+    ],
   },
   {
     county: "Kwale",
-    facilities: [],
+    facilities: [
+      {
+        name: "Kwale County Referral Hospital",
+        location: "Kwale Town",
+        phone: "+25440404040",
+        services: "General medical services, Inpatient, Outpatient",
+      },
+    ],
   },
   {
     county: "Laikipia",
-    facilities: [],
+    facilities: [
+       {
+        name: "Nanyuki Teaching and Referral Hospital",
+        location: "Nanyuki Town",
+        phone: "+254700000004",
+        services: "Referral services, Medical training",
+      },
+    ],
   },
   {
     county: "Lamu",
-    facilities: [],
+    facilities: [
+       {
+        name: "Lamu County Hospital",
+        location: "Lamu Island",
+        phone: "+25442633013",
+        services: "General medical care, Emergency services",
+      },
+    ],
   },
   {
     county: "Machakos",
-    facilities: [],
+    facilities: [
+       {
+        name: "Machakos Level 5 Hospital",
+        location: "Machakos Town",
+        phone: "+2544420235",
+        services: "Comprehensive care, Surgery, Specialized clinics",
+      },
+    ],
   },
   {
     county: "Makueni",
-    facilities: [],
+    facilities: [
+      {
+        name: "Makueni County Referral Hospital",
+        location: "Wote Town",
+        phone: "+254700000005",
+        services: "Public hospital services, Universal Health Coverage pioneer",
+      },
+    ],
   },
   {
     county: "Mandera",
-    facilities: [],
+    facilities: [
+      {
+        name: "Mandera County Referral Hospital",
+        location: "Mandera Town",
+        phone: "+254700000006",
+        services: "Public health services, Emergency care",
+      },
+    ],
   },
   {
     county: "Marsabit",
-    facilities: [],
+    facilities: [
+      {
+        name: "Marsabit County Referral Hospital",
+        location: "Marsabit Town",
+        phone: "+254692102029",
+        services: "General medical services, Inpatient care",
+      },
+    ],
   },
   {
     county: "Meru",
-    facilities: [],
+    facilities: [
+      {
+        name: "Meru Teaching & Referral Hospital",
+        location: "Meru Town",
+        phone: "+2546432103",
+        services: "Specialized medical care, Medical training",
+      },
+    ],
   },
   {
     county: "Migori",
-    facilities: [],
+    facilities: [
+       {
+        name: "Migori County Referral Hospital",
+        location: "Migori Town",
+        phone: "+254700000007",
+        services: "Public hospital services, General medical care",
+      },
+    ],
   },
   {
     county: "Mombasa",
@@ -140,11 +328,24 @@ export const countyFacilities = [
   },
   {
     county: "Murang'a",
-    facilities: [],
+    facilities: [
+      {
+        name: "Murang'a Level 5 Hospital",
+        location: "Murang'a Town",
+        phone: "+254700000008",
+        services: "Comprehensive medical services, Public health",
+      },
+    ],
   },
   {
     county: "Nairobi",
     facilities: [
+      {
+        name: "Kenyatta National Hospital",
+        location: "Hospital Road, Upper Hill",
+        phone: "+254202726300",
+        services: "Largest referral hospital, specialized care, teaching",
+      },
       {
         name: "Aga Khan University Hospital, Nairobi",
         location: "3rd Parklands Ave, Nairobi",
@@ -167,70 +368,189 @@ export const countyFacilities = [
   },
   {
     county: "Nakuru",
-    facilities: [],
+    facilities: [
+      {
+        name: "Nakuru Level 5 Hospital",
+        location: "Nakuru Town",
+        phone: "+254512217780",
+        services: "Referral hospital, Comprehensive medical care",
+      },
+    ],
   },
   {
     county: "Nandi",
-    facilities: [],
+    facilities: [
+      {
+        name: "Kapsabet County Referral Hospital",
+        location: "Kapsabet Town",
+        phone: "+2545352009",
+        services: "Public health services, General medical care",
+      },
+    ],
   },
   {
     county: "Narok",
-    facilities: [],
+    facilities: [
+       {
+        name: "Narok County Referral Hospital",
+        location: "Narok Town",
+        phone: "+2545022022",
+        services: "General medical services, Inpatient, Outpatient",
+      },
+    ],
   },
   {
     county: "Nyamira",
-    facilities: [],
+    facilities: [
+       {
+        name: "Nyamira County Referral Hospital",
+        location: "Nyamira Town",
+        phone: "+254700000009",
+        services: "Public hospital services, General care",
+      },
+    ],
   },
   {
     county: "Nyandarua",
-    facilities: [],
+    facilities: [
+       {
+        name: "Nyahururu County Referral Hospital",
+        location: "Nyahururu Town",
+        phone: "+254700000010",
+        services: "Comprehensive medical care, Public health",
+      },
+    ],
   },
   {
     county: "Nyeri",
-    facilities: [],
+    facilities: [
+      {
+        name: "Nyeri County Referral Hospital",
+        location: "Nyeri Town",
+        phone: "+254612030000",
+        services: "Specialized medical services, Referral center",
+      },
+    ],
   },
   {
     county: "Samburu",
-    facilities: [],
+    facilities: [
+      {
+        name: "Maralal County Referral Hospital",
+        location: "Maralal Town",
+        phone: "+2546562013",
+        services: "General medical services, Public health",
+      },
+    ],
   },
   {
     county: "Siaya",
-    facilities: [],
+    facilities: [
+      {
+        name: "Siaya County Referral Hospital",
+        location: "Siaya Town",
+        phone: "+254572500007",
+        services: "Public health services, HIV care, Research",
+      },
+    ],
   },
   {
     county: "Taita-Taveta",
-    facilities: [],
+    facilities: [
+      {
+        name: "Voi County Referral Hospital",
+        location: "Voi Town",
+        phone: "+254432030006",
+        services: "General medical care, Emergency services",
+      },
+    ],
   },
   {
     county: "Tana River",
-    facilities: [],
+    facilities: [
+       {
+        name: "Hola County Referral Hospital",
+        location: "Hola Town",
+        phone: "+2544632001",
+        services: "Public health services, General medical care",
+      },
+    ],
   },
   {
     county: "Tharaka-Nithi",
-    facilities: [],
+    facilities: [
+       {
+        name: "Chuka County Referral Hospital",
+        location: "Chuka Town",
+        phone: "+25464630005",
+        services: "Comprehensive medical care, Public health",
+      },
+    ],
   },
   {
     county: "Trans Nzoia",
-    facilities: [],
+    facilities: [
+      {
+        name: "Kitale County Referral Hospital",
+        location: "Kitale Town",
+        phone: "+2545431333",
+        services: "General medical care, Inpatient, Outpatient",
+      },
+    ],
   },
   {
     county: "Turkana",
-    facilities: [],
+    facilities: [
+      {
+        name: "Lodwar County Referral Hospital",
+        location: "Lodwar Town",
+        phone: "+2545421041",
+        services: "Public health services, Specialized clinics",
+      },
+    ],
   },
   {
     county: "Uasin Gishu",
-    facilities: [],
+    facilities: [
+      {
+        name: "Moi Teaching and Referral Hospital",
+        location: "Eldoret Town",
+        phone: "+254532033471",
+        services: "National referral hospital, Medical school, Cancer center",
+      },
+    ],
   },
   {
     county: "Vihiga",
-    facilities: [],
+    facilities: [
+      {
+        name: "Vihiga County Referral Hospital",
+        location: "Vihiga",
+        phone: "+254700000011",
+        services: "Public hospital services, General medical care",
+      },
+    ],
   },
   {
     county: "Wajir",
-    facilities: [],
+    facilities: [
+      {
+        name: "Wajir County Referral Hospital",
+        location: "Wajir Town",
+        phone: "+25446421020",
+        services: "General medical services, Emergency care",
+      },
+    ],
   },
   {
     county: "West Pokot",
-    facilities: [],
+    facilities: [
+       {
+        name: "Kapenguria County Referral Hospital",
+        location: "Kapenguria Town",
+        phone: "+254700000012",
+        services: "Public health services, Maternity care",
+      },
+    ],
   },
 ];
