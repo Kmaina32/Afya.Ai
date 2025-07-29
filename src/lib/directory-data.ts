4ab9f31d071d2f1b4b62385c7605da2d1e2bceae
kairo kairo <GMaina424@gmail.com>
2025-07-29 11:18:46 +0000

    now add the 47 counties in kenya to the drop down and it should be scrol

--- src/lib/directory-data.ts
+++ src/lib/directory-data.ts
@@ -1,6 +1,148 @@
 
 export const countyFacilities = [
   {
+    county: "Baringo",
+    facilities: [],
+  },
+  {
+    county: "Bomet",
+    facilities: [],
+  },
+  {
+    county: "Bungoma",
+    facilities: [],
+  },
+  {
+    county: "Busia",
+    facilities: [],
+  },
+  {
+    county: "Elgeyo-Marakwet",
+    facilities: [],
+  },
+  {
+    county: "Embu",
+    facilities: [],
+  },
+  {
+    county: "Garissa",
+    facilities: [],
+  },
+  {
+    county: "Homa Bay",
+    facilities: [],
+  },
+  {
+    county: "Isiolo",
+    facilities: [],
+  },
+  {
+    county: "Kajiado",
+    facilities: [],
+  },
+  {
+    county: "Kakamega",
+    facilities: [],
+  },
+  {
+    county: "Kericho",
+    facilities: [],
+  },
+  {
+    county: "Kiambu",
+    facilities: [],
+  },
+  {
+    county: "Kilifi",
+    facilities: [],
+  },
+  {
+    county: "Kirinyaga",
+    facilities: [],
+  },
+  {
+    county: "Kisii",
+    facilities: [],
+  },
+  {
+    county: "Kisumu",
+    facilities: [
+      {
+        name: "Jaramogi Oginga Odinga Teaching & Referral Hospital",
+        location: "Kisumu-Kakamega Road, Kisumu",
+        phone: "+254572020991",
+        services: "Public referral hospital, specialized medical training",
+      },
+      {
+        name: "Aga Khan Hospital, Kisumu",
+        location: "Otieno Oyoo Street, Kisumu",
+        phone: "+254572022153",
+        services: "Primary and specialty outpatient services.",
+      },
+    ],
+  },
+  {
+    county: "Kitui",
+    facilities: [],
+  },
+  {
+    county: "Kwale",
+    facilities: [],
+  },
+  {
+    county: "Laikipia",
+    facilities: [],
+  },
+  {
+    county: "Lamu",
+    facilities: [],
+  },
+  {
+    county: "Machakos",
+    facilities: [],
+  },
+  {
+    county: "Makueni",
+    facilities: [],
+  },
+  {
+    county: "Mandera",
+    facilities: [],
+  },
+  {
+    county: "Marsabit",
+    facilities: [],
+  },
+  {
+    county: "Meru",
+    facilities: [],
+  },
+  {
+    county: "Migori",
+    facilities: [],
+  },
+  {
+    county: "Mombasa",
+    facilities: [
+      {
+        name: "Aga Khan Hospital, Mombasa",
+        location: "Vanga Road, Mombasa",
+        phone: "+254412227710",
+        services: "General medicine, surgery, maternity, pediatrics",
+      },
+      {
+        name: "The Mombasa Hospital",
+        location: "Mama Ngina Dr, Mombasa",
+        phone: "+254722205650",
+        services: "24-hour emergency, intensive care unit, renal unit",
+      },
+    ],
+  },
+  {
+    county: "Murang'a",
+    facilities: [],
+  },
+  {
     county: "Nairobi",
     facilities: [
       {
@@ -24,39 +166,71 @@ export const countyFacilities = [
     ],
   },
   {
-    county: "Mombasa",
-    facilities: [
-      {
-        name: "Aga Khan Hospital, Mombasa",
-        location: "Vanga Road, Mombasa",
-        phone: "+254412227710",
-        services: "General medicine, surgery, maternity, pediatrics",
-      },
-      {
-        name: "The Mombasa Hospital",
-        location: "Mama Ngina Dr, Mombasa",
-        phone: "+254722205650",
-        services: "24-hour emergency, intensive care unit, renal unit",
-      },
-    ],
+    county: "Nakuru",
+    facilities: [],
   },
   {
-    county: "Kisumu",
-    facilities: [
-      {
-        name: "Jaramogi Oginga Odinga Teaching & Referral Hospital",
-        location: "Kisumu-Kakamega Road, Kisumu",
-        phone: "+254572020991",
-        services: "Public referral hospital, specialized medical training",
-      },
-      {
-        name: "Aga Khan Hospital, Kisumu",
-        location: "Otieno Oyoo Street, Kisumu",
-        phone: "+254572022153",
-        services: "Primary and specialty outpatient services.",
-      },
-    ],
+    county: "Nandi",
+    facilities: [],
   },
-  // Add other 44 counties and their facilities here...
-  // For demonstration, only a few are populated.
+  {
+    county: "Narok",
+    facilities: [],
+  },
+  {
+    county: "Nyamira",
+    facilities: [],
+  },
+  {
+    county: "Nyandarua",
+    facilities: [],
+  },
+  {
+    county: "Nyeri",
+    facilities: [],
+  },
+  {
+    county: "Samburu",
+    facilities: [],
+  },
+  {
+    county: "Siaya",
+    facilities: [],
+  },
+  {
+    county: "Taita-Taveta",
+    facilities: [],
+  },
+  {
+    county: "Tana River",
+    facilities: [],
+  },
+  {
+    county: "Tharaka-Nithi",
+    facilities: [],
+  },
+  {
+    county: "Trans Nzoia",
+    facilities: [],
+  },
+  {
+    county: "Turkana",
+    facilities: [],
+  },
+  {
+    county: "Uasin Gishu",
+    facilities: [],
+  },
+  {
+    county: "Vihiga",
+    facilities: [],
+  },
+  {
+    county: "Wajir",
+    facilities: [],
+  },
+  {
+    county: "West Pokot",
+    facilities: [],
+  },
 ];
